ca896141ae8c5595df93f0e8c3d4b9c6c5d2e353
Adam777 <Adam777-5867@outlook.com>
2022-09-28 11:57:19 +0000

    Update

--- routes/api.js
+++ routes/api.js
@@ -13,37 +13,37 @@ var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
 
 module.exports = function(app) {
-  
+
   app.route('/api/issues/:project')
   .get(function(req, res) {
     var project = req.params.project;
-    
+
     // let searchQuery = { req.query };
     let searchQuery = { project };
-    
+
     if (req.query._id !== undefined) { searchQuery['_id'] = new ObjectId(req.query._id); }
-    
+
     if (req.query.issue_title !== undefined) { searchQuery['issue_title'] = req.query.issue_title; }
-    
+
     if (req.query.issue_text !== undefined) { searchQuery['issue_text'] = req.query.issue_text; }
-    
+
     if (req.query.created_by !== undefined) { searchQuery['created_by'] = req.query.created_by; }
-    
+
     if (req.query.assigned_to !== undefined) { searchQuery['assigned_to'] = req.query.assigned_to; }
-    
+
     if (req.query.status_text !== undefined) { searchQuery['status_text'] = req.query.status_text; }
-    
+
     if (req.query.created_on !== undefined) { searchQuery['created_on'] = new Date(req.query.created_on); }
-    
+
     if (req.query.updated_on !== undefined) { searchQuery['updated_on'] = new Date(req.query.updated_on); }
-    
+
     if (req.query.open !== undefined && req.query.open === 'true') {
       searchQuery['open'] = true;
     } else if (req.query.open !== undefined && req.query.open === 'false') {
       searchQuery['open'] = false;
     }
-    
-    MongoClient.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
+
+    MongoClient.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
       if (err) {
         // console.log('Database error: ' + err);
         return res.json({ error: 'error' });
@@ -58,25 +58,25 @@ module.exports = function(app) {
     if (req.body.issue_title === undefined || req.body.issue_title === '') {
       return res.json({ error: 'Title is required' });
     }
-    
+
     if (req.body.issue_text === undefined || req.body.issue_text === '') {
       return res.json({ error: 'Text is required' });
     }
-    
+
     if (req.body.created_by === undefined || req.body.created_by === '') {
       return res.json({ error: 'Created by is required' });
     }
-    
+
     var project = req.params.project;
     let issue_title = req.body.issue_title;
     let issue_text = req.body.issue_text;
     let created_by = req.body.created_by;
     let assigned_to = (req.body.assigned_to !== undefined ? req.body.assigned_to : '');
     let status_text = (req.body.status_text !== undefined ? req.body.status_text : '');
-    
+
     let date = new Date();
-    
-    MongoClient.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
+
+    MongoClient.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
       if (err) {
         // console.log('Database error: ' + err);
         return res.json({ error: 'error' });
@@ -103,24 +103,24 @@ module.exports = function(app) {
     if (req.body._id === undefined || req.body._id === '') {
       return res.json({ error: '_id is required' });
     }
-    
+
     if (req.body.issue_title === undefined && req.body.issue_text === undefined && req.body.created_by === undefined && req.body.assigned_to === undefined && req.body.status_text === undefined && req.body.open === undefined) {
       return res.json({ error: 'no update field sent' });
     }
-    
+
     var project = req.params.project;
     let _id = req.body._id;
-    
+
     let issue_title = (req.body.issue_title !== undefined ? req.body.issue_title : '');
     let issue_text = (req.body.issue_text !== undefined ? req.body.issue_text : '');
     let created_by = (req.body.created_by !== undefined ? req.body.created_by : '');
     let assigned_to = (req.body.assigned_to !== undefined ? req.body.assigned_to : '');
     let status_text = (req.body.status_text !== undefined ? req.body.status_text : '');
     let open = (req.body.open === undefined ? true : false);
-    
+
     let date = new Date();
-    
-    MongoClient.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
+
+    MongoClient.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
       if (err) {
         // console.log('Database error: ' + err);
         return res.json({ error: 'error' });
@@ -155,14 +155,14 @@ module.exports = function(app) {
   })
   .delete(function(req, res) {
     var project = req.params.project;
-    
+
     if (req.body._id === undefined || req.body._id === '') {
       return res.json({ error: '_id error' });
     }
-    
+
     let _id = req.body._id;
-    
-    MongoClient.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
+
+    MongoClient.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
       if (err) {
         // console.log('Database error: ' + err);
         return res.json({ error: 'error' });
@@ -184,5 +184,5 @@ module.exports = function(app) {
       }
     });
   });
-  
+
 };
